9d1d65b44a91e4820e6364a60583411892bb8fd7
NaitLee <naitli@foxmail.com>
2021-07-31 18:22:59 +0800

    Multi-level Candy item & buff

---
+++ Buffs/CandyLvl3.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ChanoMod.Buffs
+{
+    class CandyLvl3 : Candy
+    {
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.lifeRegen += 7;
+            player.statDefense += 4;
+        }
+    }
+}
